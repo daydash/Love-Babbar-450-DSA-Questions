4501e8ca7458d09f29c5b4d9dadaf33f5e7001ac
daydash <thedaydash@gmail.com>
2022-04-03 10:49:58 +0530

    added test.cpp

---
+++ test.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+// 	cout << "Yash is always an OG!";
+// 	return 0;
+// }
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int n;
+
+	cin >> n;
+
+	cout << "I am daydash" << endl;
+	cout << n;
+	return 0;
+}
